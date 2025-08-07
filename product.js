26b391e18458adf97079fe9e227ff56a85467958
Jake-hphuc <dohoangphuc2806@gmail.com>
2025-08-07 12:37:15 +0700

    Add files via upload

---
+++ product.js
@@ -0,0 +1,114 @@
+const products = {
+  "runway": [
+    {
+      id: 201,
+      name: "Zegna Silk Blazer",
+      price: "25.000.000 VNĐ",
+      image: "https://www.zegna.com/dw/image/v2/BBLB_PRD/on/demandware.static/-/Sites-master-catalog/default/dw1f829dc1/images/hi-res/ZZEGNA_1.jpg",
+      description: "Áo blazer lụa Zegna tinh tế và sang trọng."
+    },
+    {
+      id: 202,
+      name: "Gucci Embroidered Gown",
+      price: "72.000.000 VNĐ",
+      image: "https://www.gucci.com/images/gucci_runway.jpg",
+      description: "Đầm Gucci thêu tay từ bộ sưu tập Runway."
+    }
+  ],
+  "timeless": [
+    {
+      id: 203,
+      name: "Chanel Tweed Jacket",
+      price: "89.000.000 VNĐ",
+      image: "https://www.chanel.com/images/tweed_jacket.jpg",
+      description: "Áo khoác tweed cổ điển – biểu tượng bất hủ của Chanel."
+    },
+    {
+      id: 204,
+      name: "Loro Piana Cashmere Coat",
+      price: "95.000.000 VNĐ",
+      image: "https://www.loropiana.com/images/cashmere_coat.jpg",
+      description: "Áo khoác Cashmere mềm mại từ Loro Piana."
+    }
+  ],
+  "modern": [
+    {
+      id: 205,
+      name: "Zegna Modern Trousers",
+      price: "18.000.000 VNĐ",
+      image: "https://www.zegna.com/images/modern_trousers.jpg",
+      description: "Quần âu hiện đại, dáng slim-fit từ Zegna."
+    },
+    {
+      id: 206,
+      name: "Gucci Monogram Bag",
+      price: "42.000.000 VNĐ",
+      image: "https://www.gucci.com/images/monogram_bag.jpg",
+      description: "Túi Gucci Monogram bản giới hạn mới nhất."
+    },
+    {
+      id: 207,
+      name: "Váy Amor",
+      price: "24.000.000 VNĐ",
+      image: "https://is4.fwrdassets.com/images/p/fw/z/GABF-WD35_V1.jpg",
+      description: "Váy Gabriela Hearst Amor."
+    },
+    {
+      id: 208,
+      name: "Váy Eyot",
+      price: "55.000.000 VNĐ",
+      image: "https://is4.fwrdassets.com/images/p/fw/z/GABF-WD64_V1.jpg",
+      description: "Váy Gabriela Hearst Eyot."
+    },
+    {
+      id: 209,
+      name: "Váy sọc",
+      price: "78.000.000 VNĐ",
+      image: "https://is4.fwrdassets.com/images/p/fw/z/MONC-WD10_V1.jpg",
+      description: "Váy sọc hiện đại."
+    },
+    {
+      id: 210,
+      name: "Váy polo mini cotton terry",
+      price: "48.996.700 VNĐ",
+      image: "https://image.celine.com/a51d1623ea454cb0/original/RR0E50A80-07YC_1_FALL25_W.tif?im=Resize=(480);AspectCrop=(1,1),xPosition=.5,yPosition=.5",
+      description: "Váy mini vải cotton terry của Celine."
+    },
+    {
+      id: 211,
+      name: "Cardigan Triomphe len Aran",
+      price: "61.245.900 VNĐ",
+      image: "https://image.celine.com/d03d3f117ad41f44/original/RV09U0117-01OW_1_SPR25_P1_W.tif?im=Resize=(2000)",
+      description: "Áo cardigan len Aran Celine."
+    },
+    {
+      id: 212,
+      name: "Áo khoác thể thao vải nhung",
+      price: "55.121.300 VNĐ",
+      image: "https://image.celine.com/d10b328c24b350c0/original/2Y16E748Q-19XC_1_SUM25_W.tif?im=Resize=(480);AspectCrop=(1,1),xPosition=.5,yPosition=.5",
+      description: "Áo khoác thể thao nhung Celine."
+    },
+    {
+      id: 213,
+      name: "Áo cánh Lavalliere Georgette",
+      price: "59.204.400 VNĐ",
+      image: "https://image.celine.com/45a021214b35eb1f/original/RB01B0393-GAH6_1_FALL25_W.tif?im=Resize=(1200);AspectCrop=(1,1),xPosition=.5,yPosition=.5",
+      description: "Áo cánh Lavalliere bằng lụa Georgette."
+    },
+    {
+      id: 214,
+      name: "Sơ mi thêu vải lanh cotton",
+      price: "56.754.600 VNĐ",
+      image: "https://image.celine.com/5e449748f1ba041d/original/RC05G0143-01AV_1_FALL25_W.tif?im=Resize=(1200);AspectCrop=(1,1),xPosition=.5,yPosition=.5",
+      description: "Áo sơ mi thêu bằng vải lanh cotton."
+    },
+    {
+      id: 215,
+      name: "Váy mohair ngắn",
+      price: "51.038.300 VNĐ",
+      image: "https://image.celine.com/4ace83518a127cd4/original/RJ03I523K-38NO_1_FALL25_W.tif?im=Resize=(1200);AspectCrop=(1,1),xPosition=.5,yPosition=.5",
+      description: "Váy ngắn bằng len mohair."
+    }
+  ]
+};
+
